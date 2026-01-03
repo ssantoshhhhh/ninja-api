8a0fc7aa08d33b34cd49b40bedf4ae0b7abaf53f
santosh <santoshkumar90101s@gmail.com>
2026-01-03 23:01:21 +0530

    HTTP methods controller

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  // GET /ninjas --> []
+@Get()
+getNinjas(@Query('type') type: string){
+  return [{type}];
+}
+
+// GET /ninjas/:id --> { ... }
+@Get(':id')
+getOneNinja(@Param(`id`) id: string){
+  return {
+    id,
+  };
+}
+
+@Post()
+createNinja(){
+  return {};
+}
+@Put(':id')
+updateNinja(@Param(`id`) id: string){
+  return {};
+}
+@Delete(':id')
+deleteNinja(@Param(`id`) id: string){
+  return {};
+}
+
+}
+
+
+// POST /ninjas
+// PUT /ninjas/:id -->  { ... }
+// DELETE /ninjas/:id 
+
